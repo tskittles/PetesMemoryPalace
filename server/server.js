0cad6fb9240106d2129d9c477cc38aa8a2683e30
Tom Rosenblatt <rosenblatttom@gmail.com>
2017-12-09 15:31:09 -0800

    some change

--- server/server.js
+++ server/server.js
@@ -149,5 +149,7 @@ app.post('/storeNode', postController.storeNode)
 
 app.get('/deleteNode', deleteController.deleteNode)
 
+app.get('/getNodes', getController.getNodes)
+
 
 app.listen(SERVER_PORT, () => console.log(`App listening on port ${SERVER_PORT}...`.yellow));
